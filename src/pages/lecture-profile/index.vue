e2209bfe8e222319b7a398e93ddbbd9beab616aa
ColdIceWater <56376624@qq.com>
2020-09-18 08:15:14 +0800

    jxy lecture-profile 2020/9/18

---
+++ src/pages/lecture-profile/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <i-panel>
+    <div class="profile">
+      <i-cell-group>
+        <i-cell title="课程编号"
+                value="B2F060110"></i-cell>
+        <i-cell title="学分/学时"
+                value="1.5/24"></i-cell>
+        <i-cell title="课程性质"
+                value="必修"></i-cell>
+        <i-cell title="开课对象"
+                value="工科试验班类（信息类）"></i-cell>
+        <i-cell title="任课教师"
+                value="熊璋，欧阳元新，蒲菊华，王静远，冷彪，盛浩，荣文戈"></i-cell>
+        <i-cell title="成绩评定"
+                value="平时成绩（70%）+ 考查大作业（30%）= 总评成绩（100%)"></i-cell>
+      </i-cell-group>
+    </div>
+
+  </i-panel>
+</template>
+ 
+
+<script>
+</script>
+
+
+<style lang="stylus" scoped>
+.header
+  text-align center
+  margin 45px auto 25px
+  >>> .avatar
+    width 100px
+    height 100px
+    border-radius 3px
+    margin 0 auto 20px
+.center
+  text-align center
+.text
+  color #80848f
+  font-size 15px
+</style>
