a409d854c94c3b7ca5225c432a3b9525cc0beffe
zsy10011998 <1002628617@qq.com>
2021-10-26 17:55:14 +0800

    修改了刷新键

--- src/pages/functions/pages/group/groupmembers/index.vue
+++ src/pages/functions/pages/group/groupmembers/index.vue
@@ -1,40 +1,41 @@
 <template>
   <div class="main-container">
-    <loop-banner
-      :texts="submitBanner"
-      :v-if="submitBanner.length"
-    />
-    <loop-banner
-      v-if="allowTimeBanner.length"
-      :texts="allowTimeBanner"
-      theme="blue"
-      :hidable="false"
-    />
+    <loop-banner :texts="submitBanner"
+                 :v-if="submitBanner.length" />
+    <loop-banner v-if="allowTimeBanner.length"
+                 :texts="allowTimeBanner"
+                 theme="blue"
+                 :hidable="false" />
     <div class="member-list">
-      <i-swipeout
-        v-for="(item, i) in membersinf"
-        :operateWidth="!groupSubmitted && isCaptain && item.status !== 'leader' ? 120 : 0"
-        :key="i"
-        class="member-item"
-      >
-        <div class="member-card" slot="content">
+      <i-swipeout v-for="(item, i) in membersinf"
+                  :operateWidth="!groupSubmitted && isCaptain && item.status !== 'leader' ? 120 : 0"
+                  :key="i"
+                  class="member-item">
+        <div class="member-card"
+             slot="content">
           <member-icon :avatarUrl="item.avatarUrl" />
           <div class="text-container">
             <div class="student-name">
               <span>{{ item.name }}</span>
-              <color-tag :text="item.text" :theme="item.theme" />
+              <color-tag :text="item.text"
+                         :theme="item.theme" />
             </div>
             <div class="student-id">
               <span>{{ item.studentNo }}</span>
-              <span v-for="timespan in item.myTimes" :key="timespan">
-                <color-tag :text="timespanMap[timespan]" :theme="timespan === allowTime ? 'purple': 'grey'" />
+              <span v-for="timespan in item.myTimes"
+                    :key="timespan">
+                <color-tag :text="timespanMap[timespan]"
+                           :theme="timespan === allowTime ? 'purple': 'grey'" />
               </span>
             </div>
           </div>
         </div>
-        <view slot="button" class="i-swipeout-demo-button-group">
-          <view class="i-swipeout-demo-button delete-button" @click="beforeRemoveMember(item)">
-            <i-icon size="32" type="delete_fill"></i-icon>
+        <view slot="button"
+              class="i-swipeout-demo-button-group">
+          <view class="i-swipeout-demo-button delete-button"
+                @click="beforeRemoveMember(item)">
+            <i-icon size="32"
+                    type="delete_fill"></i-icon>
           </view>
         </view>
       </i-swipeout>
@@ -42,27 +43,22 @@
 
     <!-- 队长操作button：添加 & 解散 & Submit -->
     <div v-if="isCaptain && !addblock && !groupSubmitted">
-      <div class="quota-list" v-if="now4 !== null && now5 !== null">
+      <div class="quota-list"
+           v-if="now4 !== null && now5 !== null">
         <div>六人组已选: <span class="now">{{now4}}</span>/<span class="max">{{max4}}</span></div>
         <div>七人组已选: <span class="now">{{now5}}</span>/<span class="max">{{max5}}</span></div>
         <div>全部已选: <span class="now">{{now4 + now5}}</span>/<span class="max">{{maxTotal}}</span></div>
       </div>
-      <button
-        hover-class="clicked"
-        class="login-button"
-        @click="displayAddBlock"
-      >添加新成员</button>
-      <button
-        hover-class="clicked"
-        class="login-button"
-        v-if="groupSubmitted === false"
-        @click="beforeSubmitGroup"
-      >提交分组</button>
-      <button
-        hover-class="clicked"
-        class="login-button"
-        @click="beforeDisGroup"
-      >解散分组</button>
+      <button hover-class="clicked"
+              class="login-button"
+              @click="displayAddBlock">添加新成员</button>
+      <button hover-class="clicked"
+              class="login-button"
+              v-if="groupSubmitted === false"
+              @click="beforeSubmitGroup">提交分组</button>
+      <button hover-class="clicked"
+              class="login-button"
+              @click="beforeDisGroup">解散分组</button>
       <!-- <button
         hover-class="clicked"
         class="login-button"
@@ -74,49 +70,38 @@
       <h1 class="menu-title">推荐成员
       </h1>
       <view class="recommend-list">
-        <view
-          class="recommend-item"
-          v-for="(item, i) in recommend"
-          :key="i"
-        >
+        <view class="recommend-item"
+              v-for="(item, i) in recommend"
+              :key="i">
           <div class="avatar-wrapper">
-            <member-icon
-              :avatarUrl="item.avatarUrl"
-              @click="addRecommendedNew(item)"
-            />
+            <member-icon :avatarUrl="item.avatarUrl"
+                         @click="addRecommendedNew(item)" />
           </div>
           <div class="name">{{item.name}}</div>
         </view>
-        <button
-          hover-class="clicked"
-          class="login-button"
-          @click="getRecommended()">换一批</button>
+        <button hover-class="clicked"
+                class="login-button"
+                @click="getRecommended()">换一批</button>
       </view>
       <h1 class="menu-title">学号添加</h1>
-      <i-input
-        type="text"
-        title="学号"
-        placeholder="输入新成员学号"
-        maxlength="20"
-        i-class="input"
-        @change="updatestudetNo"
-      />
-      <button
-        hover-class="clicked"
-        class="login-button"
-        @click="addnew(studentNo)">确认添加</button>
-      <button
-        hover-class="clicked"
-        class="login-button"
-        @click="hideAddBlock">取消</button>
+      <i-input type="text"
+               title="学号"
+               placeholder="输入新成员学号"
+               maxlength="20"
+               i-class="input"
+               @change="updatestudetNo" />
+      <button hover-class="clicked"
+              class="login-button"
+              @click="addnew(studentNo)">确认添加</button>
+      <button hover-class="clicked"
+              class="login-button"
+              @click="hideAddBlock">取消</button>
 
     </div>
-    <button
-      hover-class="clicked"
-      class="login-button"
-      @click="exitgroup"
-      v-if="isCaptain==false && !groupSubmitted"
-    >退出分组</button>
+    <button hover-class="clicked"
+            class="login-button"
+            @click="exitgroup"
+            v-if="isCaptain==false && !groupSubmitted">退出分组</button>
   </div>
 </template>
 
@@ -199,19 +184,20 @@ export default {
   onPullDownRefresh () {
     //console.log('刷新页面')
     // wx.redirectTo({ url: 'main' })
-    this.refreshData()
+    this.refreshData().then(wx.stopPullDownRefresh())
+
   },
   methods: {
-    refreshData (){
+    refreshData () {
       const { openid } = this
       const param = { openid }
       const ret1 = GetSelfGroupInfoAPI(param).then(res => {
-        const { isCaptain, isSubmit} = res
+        const { isCaptain, isSubmit } = res
         const submitBanner = []
         this.$set(this, 'isCaptain', isCaptain)
-        this.$set(this, 'groupSubmitted', isSubmit)      
-        if (!isSubmit) submitBanner.push('组长尚未提交当前分组')
+        this.$set(this, 'groupSubmitted', isSubmit)
         submitBanner.push('下拉可刷新信息')
+        if (!isSubmit) submitBanner.push('组长尚未提交当前分组')
         if (isCaptain && !isSubmit) submitBanner.push('组长可左划管理成员')
         this.$set(this, 'submitBanner', submitBanner)
       })
@@ -283,7 +269,7 @@ export default {
         this.hideAddBlock()
 
         showToast('等待学生确认')
-      }).catch(res =>{
+      }).catch(res => {
         showToast(res.errMsg)
       })
     },
@@ -304,12 +290,12 @@ export default {
       const errorMessage = !this.isAllTimespanValid()
         ? FEErrorMsg.INVALID_TIMESPAN
         : (members.filter(item => item.status === STATUS_INVITED).length > 0)
-        ? FEErrorMsg.EXIST_NOT_ACCEPTED_INVITED
-        : members.length > MAXIMUM_MEMBERS
-        ? FEErrorMsg.CANNOT_MORE_THAN_5_MEMBERS
-        : members.length < MINIMUM_MEMBERS
-        ? FEErrorMsg.CANNOT_LESS_THAN_4_MEMBERS
-        : undefined
+          ? FEErrorMsg.EXIST_NOT_ACCEPTED_INVITED
+          : members.length > MAXIMUM_MEMBERS
+            ? FEErrorMsg.CANNOT_MORE_THAN_5_MEMBERS
+            : members.length < MINIMUM_MEMBERS
+              ? FEErrorMsg.CANNOT_LESS_THAN_4_MEMBERS
+              : undefined
       if (errorMessage) {
         showToast(errorMessage)
         return
@@ -330,7 +316,7 @@ export default {
       })
     },
     exitgroup () {
-      const param = { openid: this.openid}
+      const param = { openid: this.openid }
       ExitGroupAPI(param).then(res => {
         showToast(FENoticeMsg.EXIT_SUCCESS, '').then(
           wx.redirectTo({ url: '../main' })
@@ -361,8 +347,8 @@ export default {
       const errorMessage = !this.isCaptain
         ? FEErrorMsg.CANNOT_REMOVE_BY_NON_LEADER
         : item.status === STATUS_LEADER
-        ? FEErrorMsg.CANNOT_REMOVE_LEADER
-        : undefined
+          ? FEErrorMsg.CANNOT_REMOVE_LEADER
+          : undefined
 
       if (errorMessage) {
         showToast(errorMessage)
@@ -385,7 +371,7 @@ export default {
         showToast(FENoticeMsg.REMOVE_MEMBER_SUCCESS, '')
       })
     },
-    isAllTimespanValid(toast = false) {
+    isAllTimespanValid (toast = false) {
       const { membersinf, allowTime } = this
       // const ret = false
       const ret = membersinf.every(item => item.myTimes.indexOf(allowTime) >= 0)
@@ -394,7 +380,7 @@ export default {
       }
       return ret
     },
-    testFunction() {
+    testFunction () {
       console.log(this.isAllTimespanValid())
     },
   }
@@ -402,129 +388,96 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.main-container {
-  width: 100vw;
-  min-height: 100vh;
-  position: absolute;
-  background-image: url('https://cs.zhouyc.cc/images/homeProbe.png'); // cs.zhouyc.cc/images/homeProbe.png);
-  background-position: center 80%;
-  background-repeat: no-repeat;
-  background-size: 100%;
-  background-attachment: fixed;
-}
-
-button {
-  width: 60vw;
-  background-color: $theme-blue;
-  color: white;
-  font-size: 16px;
-  margin: 20px auto;
-}
-
-.clicked {
-  background-color: $dark-blue;
-}
-
-.content {
-  margin: 20px 20px;
-}
-
-.member-list {
-  box-sizing: border-box;
-  width: 100%;
-  padding: 25rpx;
-}
-
-.member-list .member-card {
-  box-sizing: border-box;
-  position: relative;
-  width: 100%;
-  float: left;
-  padding: 0 10rpx;
-  display: flex;
-  flex-direction: row;
-  background: rgba(255, 255, 255, 0.8);
-}
-
-.image-container {
-  height: 120rpx;
-  width: 120rpx;
-  border-radius: 50%;
-  overflow: hidden;
-
-  >>>image {
-    width: 100%;
-    height: 100%;
-  }
-}
-
-.text-container {
-  max-width: 400rpx;
-  padding-left: 20rpx;
-}
-
-.student-name {
-  font-weight: bold;
-  font-size: 36rpx;
-  position: relative;
-}
-
-.student-id {
-  margin-top: 12rpx;
-  font-size: 24rpx;
-  color: #555;
-}
-
-.delete-button {
-  background: #ff5252;
-  color: #ffffff;
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.quota-list {
-  margin: 0 25rpx;
-  font-size: 28rpx;
-  color: #777;
-  line-height: 40rpx;
-  position: relative;
-  background: rgba(255,255,255,0.8);
-  padding: 15px 20px;
-}
-
-.recommend-list {
-  white-space: nowrap;
-  overflow: auto;
-  margin: 0 45rpx;
-  background: rgba(255,255,255,0.8);
-}
-
-.recommend-item {
-  display: inline-block;
-  width: 200rpx;
-}
-
-.avatar-wrapper {
-  padding-left: 40rpx
-}
-.name {
-  text-align: center;
-  font-weight: bold;
-  color: #555;
-  margin-top: 10rpx;
-}
-.menu-title {
-  color: rgb(28, 133, 185);
-  letter-spacing: 1px;
-  font-size: 30rpx;
-  font-weight: bolder;
-  margin: 30rpx 0 30rpx 10rpx;
-}
+.main-container
+  width 100vw
+  min-height 100vh
+  position absolute
+  background-image url('https://cs.zhouyc.cc/images/homeProbe.png') // cs.zhouyc.cc/images/homeProbe.png);
+  background-position center 80%
+  background-repeat no-repeat
+  background-size 100%
+  background-attachment fixed
+button
+  width 60vw
+  background-color $theme-blue
+  color white
+  font-size 16px
+  margin 20px auto
+.clicked
+  background-color $dark-blue
+.content
+  margin 20px 20px
+.member-list
+  box-sizing border-box
+  width 100%
+  padding 25rpx
+.member-list .member-card
+  box-sizing border-box
+  position relative
+  width 100%
+  float left
+  padding 0 10rpx
+  display flex
+  flex-direction row
+  background rgba(255, 255, 255, 0.8)
+.image-container
+  height 120rpx
+  width 120rpx
+  border-radius 50%
+  overflow hidden
+  >>>image
+    width 100%
+    height 100%
+.text-container
+  max-width 400rpx
+  padding-left 20rpx
+.student-name
+  font-weight bold
+  font-size 36rpx
+  position relative
+.student-id
+  margin-top 12rpx
+  font-size 24rpx
+  color #555
+.delete-button
+  background #ff5252
+  color #ffffff
+  height 100%
+  width 100%
+  position absolute
+  top 50%
+  left 50%
+  transform translate(-50%, -50%)
+  display flex
+  align-items center
+  justify-content center
+.quota-list
+  margin 0 25rpx
+  font-size 28rpx
+  color #777
+  line-height 40rpx
+  position relative
+  background rgba(255, 255, 255, 0.8)
+  padding 15px 20px
+.recommend-list
+  white-space nowrap
+  overflow auto
+  margin 0 45rpx
+  background rgba(255, 255, 255, 0.8)
+.recommend-item
+  display inline-block
+  width 200rpx
+.avatar-wrapper
+  padding-left 40rpx
+.name
+  text-align center
+  font-weight bold
+  color #555
+  margin-top 10rpx
+.menu-title
+  color rgb(28, 133, 185)
+  letter-spacing 1px
+  font-size 30rpx
+  font-weight bolder
+  margin 30rpx 0 30rpx 10rpx
 </style>
